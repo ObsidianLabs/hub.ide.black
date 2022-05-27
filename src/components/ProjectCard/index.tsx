ff734e3c86bffa8f8023fb5169207a8c5741d29f
iNorthIsle <ndobox@outlook.com>
2022-05-27 20:04:19 +0800

    fix: image service

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -10,16 +10,14 @@ const ProjectCard = ({ projectName, createTime, userInfo }: any) => {
 		<div className='relative w-full overflow-hidden duration-300 ease-out border border-gray-800 rounded-md cursor-pointer hover:scale-105'>
 			<a href={`https://ide.black/${userInfo.username}/${projectName}`} target='_blank'>
 				<div>
-					<div className='relative'>
-						<Image src={`${process.env.NEXT_PUBLIC_OG_IMG_SERVICE_URL}/${projectName}.png`} layout='responsive' width='100%' height='60px' />
+					<div className='relative aspect-video'>
+						<img className='aspect-video' src={`${process.env.NEXT_PUBLIC_OG_IMG_SERVICE_URL}/${projectName}.png`} />
 					</div>
 					<div className='px-2 py-2 border-t-2 border-secondary'>
-						<h3 className='text-base text-white cursor-pointer hover:text-primary overflow-hidden whitespace-nowrap text-ellipsis'>{projectName}</h3>
+						<h3 className='overflow-hidden text-base text-white cursor-pointer hover:text-primary whitespace-nowrap text-ellipsis'>{projectName}</h3>
 						<div className='flex justify-between mt-4 mb-2 text-sm text-gray-500'>
 							<div className='flex items-center'>
-								<div className='relative w-4 h-4 mr-2 overflow-hidden rounded-full'>
-									<Image className="inline-block w-6 h-6 ring-1 ring-white" layout='fill' src={userInfo.avatar} />
-								</div>
+								<img className="inline-block w-4 h-4 mr-2 rounded-full ring-1 ring-white aspect-square" src={userInfo.avatar} />
 								<span className='font-medium'>{userInfo.username}</span>
 							</div>
 							<span className='font-medium'>
