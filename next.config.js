78a3695ce4e3ce025a1fccf6c7281acb38c3927d
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-05-27 21:16:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  crossOrigin: 'anonymous'.
   images: {
     domains: ['og-image-service-ten.vercel.app', 'avatars.githubusercontent.com'],
   },
